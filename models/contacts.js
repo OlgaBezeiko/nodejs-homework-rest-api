18063103248cf24b9bce0dff1c6f6a15eda54271
OlgaBezeiko <122458798+OlgaBezeiko@users.noreply.github.com>
2023-10-08 16:55:59 +0300

    finish fix

--- models/contacts.js
+++ models/contacts.js
@@ -1,7 +1,8 @@
 
 const fs = require('fs/promises');
 const path = require('path');
-//const { nanoid } = require('nanoid');
+
+const { nanoid } = require('nanoid');
 
 const contactsPath = path.join(__dirname, 'contacts.json');
 
