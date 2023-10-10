63c4e071f1f4a09968825366e03dc718f21614e6
OlgaBezeiko <122458798+OlgaBezeiko@users.noreply.github.com>
2023-10-10 22:09:39 +0300

    +

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -7,7 +7,7 @@ const { addSchema } = require('../../schemas/contacts');
 const router = express.Router();
 
 router.get('/', ctrl.listContacts);
-
+ 
 router.get('/:contactId', ctrl.getById);
 
 router.delete('/:contactId', ctrl.removeContact);
@@ -17,3 +17,5 @@ router.post('/', validateBody(addSchema), ctrl.addContact);
 router.put('/:contactId', validateBody(addSchema), ctrl.updateContact);
 
 module.exports = router;
+
+
