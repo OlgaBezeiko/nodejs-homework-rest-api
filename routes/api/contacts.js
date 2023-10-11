8711599e8b1e69b4a389a057c01bd23831bd7e5b
OlgaBezeiko <122458798+OlgaBezeiko@users.noreply.github.com>
2023-10-11 21:53:43 +0300

    ++

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,21 +1,32 @@
-const express = require('express');
+const express = require("express");
 
-const ctrl = require('../../controllers/contacts');
-const { validateBody } = require('../../middlewares/validateBody');
-const { addSchema } = require('../../schemas/contacts');
+const ctrl = require("../../controllers/contacts");
+const { validateBody } = require("../../middlewares");
+const { schemas } = require("../../models");
+const { isValidId } = require("../../middlewares");
 
 const router = express.Router();
 
-router.get('/', ctrl.listContacts);
- 
-router.get('/:contactId', ctrl.getById);
+router.get("/", ctrl.listContacts);
 
-router.delete('/:contactId', ctrl.removeContact);
+router.get("/:contactId", isValidId, ctrl.getById);
 
-router.post('/', validateBody(addSchema), ctrl.addContact);
+router.delete("/:contactId", isValidId, ctrl.removeContact);
 
-router.put('/:contactId', validateBody(addSchema), ctrl.updateContact);
+router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
+
+router.put(
+  "/:contactId",
+  isValidId,
+  validateBody(schemas.addSchema),
+  ctrl.updateContact
+);
+
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validateBody(schemas.updFavoriteSchema),
+  ctrl.updateFavorite
+);
 
 module.exports = router;
-
-
